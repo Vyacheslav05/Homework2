f8dc74710b1a5ab866861fc5892a12e2c2280c8b
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-06-03 16:18:41 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+task_qty = 12
+hours_spent  = 1.5
+course_name = 'Python'
+time_to_1_task = (hours_spent / task_qty)
+print('Курс ',course_name,', всего задач:', task_qty, ', затрачено часов: ', hours_spent, ', среднее время выполнения ', time_to_1_task, ' часа.' )
